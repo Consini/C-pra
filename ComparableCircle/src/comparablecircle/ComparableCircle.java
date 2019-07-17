b046e31ea9ec6074335a65d839cb9f5fe1177683
Consini <2459971164@qq.com>
2019-07-17 14:49:07 +0800

    ComparableCircle

---
+++ ComparableCircle/src/comparablecircle/ComparableCircle.java
@@ -0,0 +1,20 @@
+
+package comparablecircle;
+import java.util.Scanner;
+public class ComparableCircle {   
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("请输入一个整数:");
+        long num=input.nextLong();
+        System.out.println(num+"各位数字之和"+sumDi(num));
+    }
+    public static int sumDi(long n)
+    {
+        int tmp=0;
+        while(n!=0){
+            tmp+=n%10;
+            n/=10;
+        }
+        return tmp;
+    }
+}
