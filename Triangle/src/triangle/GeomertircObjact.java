9484cb6dcc4445b73a8ddedde7a451b4761e1c15
Consini <2459971164@qq.com>
2019-07-18 15:19:25 +0800

    Triangle

---
+++ Triangle/src/triangle/GeomertircObjact.java
@@ -0,0 +1,28 @@
+//package triangle;
+//import java.util.Date;
+//public class GeomertircObjact {
+//private String color = null;
+//private boolean filled = false;
+//private java.util.Date datacreated;  
+//public GeomertircObjact(){
+//    datacreated = new Date();
+//}
+//public GeomertircObjact(String color, boolean filled){
+//    this.color = color;
+//    this.filled = filled;
+//    datacreated = new Date();
+//} 
+//public boolean isFilled() {
+//    if(this.color != null) filled = true;
+//    return filled;
+//}
+//public void setcolor(String color) {
+//    this.color = color;
+//}
+//public String getcolor() {
+//    return color;
+//}
+//public java.util.Date getDataCreated(){
+//    return datacreated;
+//}
+//}
