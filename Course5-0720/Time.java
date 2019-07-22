2a95aaf94cc4011d118e5ef3c149bf435d29c3d6
Consini <2459971164@qq.com>
2019-07-22 16:16:07 +0800

    Time

--- Course5-0720/Time.java
+++ Course5-0720/Time.java
@@ -9,58 +9,83 @@
  *		3） 多少秒之前的时/分/秒
  */
 public class Time {
-	public int hour = 0;
-	public int minute = 0;
-	public int second = 0;
+	private int hour = 0;
+	private int minute = 0;
+	private int second = 0;
 	
 	public Time (int hour,int minute,int second){
 		if(hour < 0 || hour > 23){
-			System.out.println("小时不对");
+			System.err.println("小时不对");
+			return ;
 		}
 		
 		if(minute < 0 || minute > 59){
-			System.out.println("分钟不对");
+			System.err.println("分钟不对");
+			return;
 		}
 		
 		if(second < 0 || second > 59){
-			System.out.println("秒不对");
-
+			System.err.println("秒不对");
+			return;
 		}
 		this.hour = hour;
 		this.minute = minute;
 		this.second = second;
 	}
+	
+	
 	//计算之后的
-	public Time after(int seconds){
-		second += seconds;
-		while(second > 59){
-			second -= 60;
-			minute++;
-			if(minute > 59){
-				minute = 0;
-				hour++;//小时加1，分钟从0开始
+	public Time immutableAfter(int seconds){
+		if(seconds < 0){
+			System.out.println("不允许小于0！");
+			return null;
+		}
+		Time other = new Time(hour, minute, second);
+		other.second += seconds;
+		while(other.second > 59){
+			other.second -= 60;
+			other.minute += 1;
+			if(other.minute > 59){
+				other.minute = 0;
+				other.hour += 1;//小时加1，分钟从0开始
 			}
-			if(hour > 23){
-				hour = 0;
+			if(other.hour > 23){
+				other.hour = 0;
 			}
 		}
-		return this;
+		return other;
 	}
 	//计算之前的
-	public Time before(int seconds){
-		second += seconds;
-		while (second < 0){
-			second += 60;
-			minute--;
-			if(minute < 0){
-				minute = 59;
-				hour--;//小时减1，分钟从59开始
+	public Time immutableBefore(int seconds){
+		if(seconds > 0){
+			System.out.println("不允许大于0！");
+			return null;
+		}
+		Time other = new Time(hour, minute, second);
+		other.second += seconds;
+		while (other.second < 0){
+			other.second += 60;
+			other.minute--;
+			if(other.minute < 0){
+				other.minute = 59;
+				other.hour--;//小时减1，分钟从59开始
 			}
-			if(hour < 0){
-				hour = 23;
+			if(other.hour < 0){
+				other.hour = 23;
 			}
 		}
-		return this;
+		return other;
+	}
+	//相差多少秒
+	public static int diff1(Time a,Time b){
+		return (a.hour - b.hour)*3600 + (a.minute - b.minute)*60 + (a.second - b.second);
+	}
+	public static Time diff2(Time a,Time b){
+		Time diff = new Time(0,0,0);
+		diff.hour = (a.hour - b.hour);
+		diff.minute = (a.minute - b.minute);
+		diff.second = (a.second - b.second);
+		return  diff;
 	}
 	
 	public String toString(){
@@ -68,10 +93,13 @@ public class Time {
 	}
 	
 	public static void main(String[] args){
-		Time t = new Time(23,02,10);
-		//Time r = t.after(1200);
-		Time r = t.before(-290);
-		System.out.println(r.toString());
+		Time now = new Time(23,20,10);
+		Time after = now.immutableAfter(200);
+		Time before = now.immutableBefore(-290);
+		System.out.println(after.toString());
+		System.out.println(before.toString());
+		System.out.println(diff1(before,after));
+		System.out.println(diff2(before,after).toString());
 	}
 	
 }
