67391dfa95f29f1a570257dae68ff8ed7184f5a9
Consini <2459971164@qq.com>
2019-07-26 22:45:32 +0800

    链表

--- LinkList0725/src/LinkList.java
+++ LinkList0725/src/LinkList.java
@@ -1,5 +1,7 @@
 import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
 
+import java.util.List;
+
 /**
  * @ClassName LinkList
  * @Description:
@@ -32,5 +34,63 @@ public class LinkList {
         pos.next = node;
     }
 
+    public Node removeAll(Node head, int val){
+        Node tmpHead = new Node(-1);
+        tmpHead.next = head;
+        Node prev = tmpHead;
+        Node result = head;
+        while(result != null){
+            if(result.val == val){
+                prev.next = result.next;
+            }else{
+                prev = result;
+            }
+            result = result.next;
+        }
+        return tmpHead.next;
+    }
+    public Node merge(Node h1,Node h2){
+        if(h1 == null){
+            return h2;
+        }
+        if(h2 == null){
+            return h1;
+        }
+        Node last = null;
+        Node result = null;
+        while(h1 != null && h2 != null){
+            if(h1.val <= h2.val){
+
+                if(result == null){
+                    result = h1;
+                }else{
+                    last.next = h1;
+                }
+                last = h1;
+                h1 = h1.next;
+            }else{
+
+                if(result == null){
+                    result = h2;
+                }else{
+                    last.next = h2;
+                }
+                last = h2;
+                h2 = h2.next;
+            }
+
+        }
+        if(h1 == null){
+            last.next = h2;
+        }
+        if(h2 == null){
+            last.next = h1;
+        }
+        return null;
+    }
+
+public Node partition(Node head,int x){
+    return null;
+}
 
 }
