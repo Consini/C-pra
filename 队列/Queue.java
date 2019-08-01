be01da8a97b56386df9b85026646e190ce91d0a3
Consini <2459971164@qq.com>
2019-08-01 22:20:57 +0800

    队列

---
+++ 队列/Queue.java
@@ -0,0 +1,45 @@
+Integer.MAX_VALUE  静态属性
+Integer.pr……      静态方法
+   
+   1.大括号，定义变量需要作用域
+   2.ArrayList <不能为基本数据类型，需要用基本数据类型的包装类>
+   int --> Integer
+   ……
+class Solution {
+    public boolean isValid(String s) {
+		
+        for(int i = 0;i < s.length();i++){
+			char c = a.charAt(i);
+		
+				
+		}
+    }
+}
+   
+class MyQueue {
+	private ArrayList<Integer> out;
+	private ArrayList<Integer> in;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        
+    }
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        
+    }
+}
