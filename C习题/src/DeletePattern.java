0acd9a9342d190e77ff92bf0230f1c842236dbb0
Consini <2459971164@qq.com>
2019-08-12 23:05:28 +0800

    从第一个字符串中删除第二个字符串内容

---
+++ C习题/src/DeletePattern.java
@@ -0,0 +1,35 @@
+import java.net.SocketTimeoutException;
+
+/**
+ * @ClassName DeletePattern
+ * @Description:
+ * @Author: 寇明珠
+ * @Date: 2019/8/12
+ * @Time: 19:08
+ * @Version 1.0
+ **/
+
+public class DeletePattern {
+    public static char[] deletePattern1(char[] a, char[] b){
+       for(int i = 0;i < b.length;i++){
+           for(int j = 0;j < a.length;j++){
+               if(a[j] == b[i]){
+                   a[j] = 0;
+               }
+           }
+       }
+       return a;
+    }
+    public static void main(String[] args) {
+        String a = "abcdeabc";
+        char[] a1 = a.toCharArray();
+        String b = "abc";
+        char[] b1 = b.toCharArray();
+        deletePattern1(a1,b1);
+        for(int i = 0;i < a1.length;i++){
+            if(a1[i] != 0){
+                System.out.printf("%s",a1[i]);
+            }
+        }
+    }
+}
