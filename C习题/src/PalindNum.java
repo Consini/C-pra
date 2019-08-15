1f23ea9dd1e3b2bc8a485b996c1c6acec8b9dc75
Consini <2459971164@qq.com>
2019-08-16 00:07:11 +0800

    回文数

--- C习题/src/PalindNum.java
+++ C习题/src/PalindNum.java
@@ -7,27 +7,48 @@
  * @Version 1.0
  **/
 public class PalindNum {
-    public static boolean isPalindrome(int x) {
-        if(x < 11){
-            return false;
-        }
-        int[] a = new int[10];
-        int j = 0;
-        while(x != 0){
-            a[j] = x % 10;
-            x /= 10;
-            j += 1;
-        }
-        for(int i = 0;i < a.length / 2;i++){
-            if(a[i] != a[a.length - 1 - i]){
+    //首位末位比较
+    public static boolean isPalindrome1(int x) {
+            if(x < 0){
                 return false;
             }
+            int div = 1;
+            while(x / div >= 10){
+                div *= 10;
+            }
+            while(x > 0){
+                if(x/div != x%10){
+                    return false;
+                }
+                x = (x%div)/10;
+                div /= 100;
+            }
+            return true;
         }
-        return true;
+    //旋转后一半数字
+    public static boolean isPalindrome2(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)){
+            return false;
+        }
+        int num = 0;
+        while(x > num){
+            num = num * 10 + x % 10;
+            x /= 10;
+        }
+        return x == num || x == (num / 10);
     }
+
     public static void main(String[] args) {
-        if(isPalindrome(121)){
+        if(isPalindrome1(1231)){
             System.out.println("Is PrlindNum!");
+        }else{
+            System.out.println("Not PrlindNum");
+        }
+
+        if(isPalindrome2(121)){
+            System.out.println("Is PrlindNum!");
+        }else{
+            System.out.println("Not PrlindNum");
         }
     }
 }
