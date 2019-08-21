55c786efe6fb8b8ff36228b86f238fd0bff9db83
Consini <2459971164@qq.com>
2019-08-21 21:08:31 +0800

    从第一个字符串中删除第二个字符串中的内容

--- C习题/src/DeletePattern.java
+++ C习题/src/DeletePattern.java
@@ -3,33 +3,28 @@ import java.net.SocketTimeoutException;
 /**
  * @ClassName DeletePattern
  * @Description:
- * @Author: 寇明珠
  * @Date: 2019/8/12
  * @Time: 19:08
  * @Version 1.0
  **/
-
+import java.lang.StringBuilder;
 public class DeletePattern {
-    public static char[] deletePattern1(char[] a, char[] b){
-       for(int i = 0;i < b.length;i++){
-           for(int j = 0;j < a.length;j++){
-               if(a[j] == b[i]){
-                   a[j] = 0;
-               }
-           }
-       }
-       return a;
-    }
-        public static void main(String[] args) {
-        String a = "abcdeabc";
-        char[] a1 = a.toCharArray();
-        String b = "abc";
-        char[] b1 = b.toCharArray();
-        deletePattern1(a1,b1);
-        for(int i = 0;i < a1.length;i++){
-            if(a1[i] != 0){
-                System.out.printf("%s",a1[i]);
+    public static String deletePattern(String a,String b){
+        StringBuilder s = new StringBuilder();//存储结果字符串
+        char[] b1 = new char[256];
+        for(int i = 0;i < b.length();i++){
+            b1[b.charAt(i) ] = 1;
+        }
+        for(int i = 0;i < a.length();i++){
+            if(b1[a.charAt(i)] != 1){
+                s.append(a.charAt(i));
             }
         }
+        return s.toString();
+    }
+        public static void main(String[] args) {
+        String a = "They are students.";
+        String b = "aeiou";
+        System.out.println(deletePattern(a,b));
     }
 }
