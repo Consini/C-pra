2731d3693ae361f7565c5a34690744d699384e99
Consini <2459971164@qq.com>
2019-09-16 21:46:42 +0800

    二叉树的基本操作

--- 09-14/src/BinaryTree.java
+++ 09-14/src/BinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @ClassName BinaryTree
  * 二叉树
@@ -38,51 +41,108 @@ public class BinaryTree {
         c.right = g;
         return a;
     }
-    //前序遍历
-    public static void preorderTraversal(Node root){
+    //前序遍历1:递归
+    public static void preorderTraversal1(Node root){
         if(root == null){
             return ;
         }
         System.out.println(root);
-        preorderTraversal(root.left);
-        preorderTraversal(root.right);
+        preorderTraversal1(root.left);
+        preorderTraversal1(root.right);
     }
-    //中序遍历
-    public static void inorderTraversal(Node root){
+    // 前序遍历2：汇总
+    public static List<Character> preorderTraversal2(Node root){
+        if(root == null){
+            return new ArrayList<>();
+        }
+        List<Character> list = new ArrayList<>();
+        list.add(root.val);
+        list.addAll(preorderTraversal2(root.left));
+        list.addAll(preorderTraversal2(root.right));
+        return list;
+    }
+    //中序遍历1:递归
+    public static void inorderTraversal1(Node root){
         if(root == null){
             return;
         }
-       inorderTraversal(root.left);
+       inorderTraversal1(root.left);
         System.out.println(root);
-       inorderTraversal(root.right);
+       inorderTraversal1(root.right);
     }
-    // 后序遍历
-    public static void postorderTraversal(Node root){
+    // 中序遍历2：汇总
+    public static List<Character> inorderTraversal2(Node root){
+        if(root == null){
+            return new ArrayList<>();
+        }
+        List<Character> list = new ArrayList<>();
+        list.addAll(inorderTraversal2(root.left));
+        list.add(root.val);
+        list.addAll(inorderTraversal2(root.right));
+        return list;
+    }
+    // 后序遍历1:递归
+    public static void postorderTraversal1(Node root){
         if(root == null){
             return;
         }
-        postorderTraversal(root.left);
-        postorderTraversal(root.right);
+        postorderTraversal1(root.left);
+        postorderTraversal1(root.right);
         System.out.println(root);
+    }
+    //后序遍历2：汇总
+    public static List<Character> postorderTraversal2(Node root){
+        if(root == null){
+            return new ArrayList<>();
+        }
+        List<Character> list = new ArrayList<>();
+        list.addAll(postorderTraversal2(root.left));
+        list.addAll(postorderTraversal2(root.right));
+        list.add(root.val);
+        return list;
     }
     // 遍历思路-求结点个数
-    public static int size = 0;
-//    public static void getSize1(Node root){
-//
-//    }
-//    // 子问题思路-求结点个数,汇总
-//    public static int getSize2(Node root){
-//
-//    }
-//    // 遍历思路-求叶子结点个数
-//    public static int leafSize = 0;
-//    public static void getLeafSize1(Node root){
-//
-//    }
-    // （汇总）子问题思路-求叶子结点个数
-//    public static int getLeafSize2(Node root){
-//
-//    }
+    public static int count = 0;
+    public static void getSize1(Node root){
+        if(root == null){
+            return ;
+        }
+        count++;
+        getSize1(root.left);
+        getSize1(root.right);
+    }
+    // 子问题思路-求结点个数,汇总
+    public static int getSize2(Node root){
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return 1;
+        }
+        return getSize2(root.left) + getSize2(root.right) + 1;
+    }
+    // 遍历思路-求叶子结点个数
+    public static int leafSize = 0;
+    public static void getLeafSize1(Node root){
+        if(root == null){
+            return;
+        }
+        if(root.left == null && root.right == null){
+            leafSize++;
+        }
+        getLeafSize1(root.left);
+        getLeafSize1(root.right);
+    }
+     //（汇总）子问题思路-求叶子结点个数
+    public static int getLeafSize2(Node root){
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return 1;
+        }
+        return getLeafSize2(root.left) + getLeafSize2(root.right);
+    }
     // （汇总）子问题思路-求第 k 层结点个数
     public static int getKLevelSize(Node root,int k){
         if(root == null){
@@ -138,16 +198,29 @@ public class BinaryTree {
     }
     public static void main(String[] args) {
         Node root = buildTree();
-        preorderTraversal(root);
+        System.out.println("前序遍历为：");
+        preorderTraversal1(root);
         System.out.println("----------");
-        inorderTraversal(root);
+        System.out.println(preorderTraversal2(root));
+        System.out.println("中序遍历为：");
+        inorderTraversal1(root);
         System.out.println("----------");
-        postorderTraversal(root);
+        System.out.println(inorderTraversal2(root));
+        System.out.println("后序遍历为：");
+        postorderTraversal1(root);
         System.out.println("----------");
+        System.out.println(postorderTraversal2(root));
+        System.out.println("----------");
+        count = 0;//每次清零，否则1下次会从原来结果上继续加
+        getSize1(root);
+        System.out.println("节点个数为： "+ count);
+        System.out.println("节点个数为： "+getSize2(root));
+        leafSize = 0;
+        getLeafSize1(root);
+        System.out.println("叶子节点个数为： "+ leafSize);
+        System.out.println("叶子节点个数为： "+getLeafSize2(root));
         System.out.println("第4层结点个数为："+getKLevelSize(root,4));
-        System.out.println("----------");
         System.out.println("树的高度为："+getHeight(root));
-        System.out.println("----------");
         System.out.println(find(root,'F'));
         System.out.println("----------");
         System.out.println(find2(root,'p'));
