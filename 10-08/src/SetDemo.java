d338050c5dc0255d262f7d27ceb65787bd7c152b
Consini <2459971164@qq.com>
2019-10-11 18:51:42 +0800

    Set练习

---
+++ 10-08/src/SetDemo.java
@@ -0,0 +1,27 @@
+/**
+ * @ClassName SetDemo
+ * @Author: K
+ * @create: 2019/10/10-17:39
+ **/
+import java.util.Set;
+import java.util.TreeSet;
+public class SetDemo {
+    public static void main(String[] args) {
+        Set<String> bucket = new TreeSet<>();
+        bucket.add("苹果");
+        bucket.add("香蕉");
+        bucket.add("橘子");
+        bucket.add("石榴");
+        bucket.add("柿子");
+        System.out.println(bucket +" "+ bucket.size());
+        System.out.println(bucket.add("橘子"));
+        System.out.println(bucket.remove("苹果"));
+        System.out.println(bucket +" "+ bucket.size());
+        System.out.println(bucket.remove("火龙果"));
+        System.out.println(bucket +" "+ bucket.size());
+        System.out.println(bucket.contains("香蕉"));
+        bucket.clear();
+        System.out.println(bucket +" "+ bucket.size());
+    }
+}
+
