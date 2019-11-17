4f94aaa59bd0da89f54cdf57975073e740bb9083
Consini <2459971164@qq.com>
2019-11-17 11:47:56 +0800

    按序打印

---
+++ 11-09多线程/src/按序打印/fun1.java
@@ -0,0 +1,73 @@
+package 按序打印;
+/**
+ * @Description TODO
+ * @Author K
+ * @Date 2019/11/17 11:24
+ **/
+//0 代表A，1代表 B ，2 代表 C
+
+public class fun1 {
+    private static class Foo {
+        private static int n = 0;
+        public void one() {//只有 n == 0 才继续执行，不为 0 是就退出，然后继续抢锁进行判断
+            synchronized (this) {
+                if (n != 0) {
+                    return;
+                }
+                System.out.println("one");
+                n = 1;
+            }
+        }
+        public void two() {
+            synchronized (this) {
+                if (n != 1) {
+                    return;
+                }
+                System.out.println("two");
+                n = 2;
+            }
+        }
+        public void three() {
+            synchronized (this) {
+                if (n != 2) {
+                    return;
+                }
+                System.out.println("three");
+                n = 0;
+            }
+        }
+    }
+    private static Foo foo = new Foo();
+    private static class OneThread extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                foo.one();
+            }
+        }
+    }
+    private static class TwoThread extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                foo.two();
+            }
+        }
+    }
+    private static class ThreeThread extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                foo.three();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Thread one = new OneThread();
+        Thread two = new TwoThread();
+        Thread three = new ThreeThread();
+        one.start();
+        two.start();
+        three.start();
+    }
+}
---
+++ 11-09多线程/src/按序打印/fun3.java
@@ -0,0 +1,71 @@
+package 按序打印;
+
+/**
+ * @Description TODO
+ * @Author K
+ * @Date 2019/11/17 11:41
+ **/
+public class fun3 {
+    private static class Foo {
+        private volatile int n = 0;
+        public void one() {
+                if (n != 0) {
+                    return;
+                }
+                System.out.println("one");// 和 n=1 顺序不能交换，线程调度可能发生在任何时期
+                n = 1;
+        }
+
+        public void two() {
+                if (n != 1) {
+                    return;
+                }
+                System.out.println("two");
+                n = 2;
+        }
+
+        public void three() {
+                if (n != 2) {
+                    return;
+                }
+                System.out.println("three");
+                n = 0;
+        }
+        private static Foo foo = new Foo();
+        private static class OneThread extends Thread {
+            @Override
+            public void run() {
+                while (true) {
+                    foo.one();
+                }
+            }
+        }
+
+        private static class TwoThread extends Thread {
+            @Override
+            public void run() {
+                while (true) {
+                    foo.two();
+                }
+            }
+        }
+
+        private static class ThreeThread extends Thread {
+            @Override
+            public void run() {
+                while (true) {
+                    foo.three();
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            Thread one = new OneThread();
+            Thread two = new TwoThread();
+            Thread three = new ThreeThread();
+            one.start();
+            two.start();
+            three.start();
+        }
+    }
+}
