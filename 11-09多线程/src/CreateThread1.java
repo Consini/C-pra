bb5129ee8939a410791e35dcaa5c8622fd39e07d
Consini <2459971164@qq.com>
2019-11-24 21:55:48 +0800

    创建线程

---
+++ 11-09多线程/src/CreateThread1.java
@@ -0,0 +1,69 @@
+/**
+ * @Description TODO
+ * @Author K
+ * @Date 2019/11/9 17:44
+ **/
+public class CreateThread1 {
+    private static class MyThread extends Thread {
+        private int n = 0;
+
+        @Override
+        public void run() {
+            while (n < 10) {
+                System.out.println(n);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                n++;
+            }
+        }
+    }
+
+    private static class ShareResource {
+        public int n = 0;
+    }
+
+    private static class MyThread2 extends Thread {
+        private ShareResource resource;
+
+        MyThread2(ShareResource resource) {
+            this.resource = resource;
+        }
+
+        @Override
+        public void run() {
+            while (resource.n < 10) {
+                System.out.println(resource.n);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                resource.n++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        /*
+        Thread thread1 = new MyThread();//没有共享
+        Thread thread2 = new MyThread();
+         */
+        /*
+        Thread thread1 = new Thread(new MyThread());//没有共享
+        Thread thread2 = new Thread(new MyThread());
+         */
+        /*
+        MyThread runnable = new MyThread();//有共享
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+         */
+        ShareResource resource = new ShareResource();//有共享
+        Thread thread1 = new MyThread2(resource);
+        Thread thread2 = new MyThread2(resource);
+        thread1.start();
+        thread2.start();
+    }
+}
