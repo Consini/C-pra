6e49da597731af597dddde52a5d42d8a6f20d40d
Consini <2459971164@qq.com>
2019-12-01 13:28:17 +0800

    文件相关方法操作练习

---
+++ 文件/src/about_File.java
@@ -0,0 +1,60 @@
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @Description TODO
+ * @Author K
+ * @Date 2019/11/29 18:47
+ **/
+public class about_File {
+    public static void main(String[] args) throws IOException {
+//        File[] file = File.listRoots();// 获得所有根目录
+//        for(File f:file){
+//            System.out.println(f);
+//            System.out.println(f.exists());
+//        }
+
+//        System.out.println(System.getProperty("user.dir"));//获得当前运行目录
+
+//        String path = "相对路径.txt";
+//        File file = new File(path);
+//        file.createNewFile();
+
+//        String path = "F:\\GitHub\\JavaSE\\测试文件目录\\隐藏文件.txt";
+//        File file = new File(path);
+//        System.out.println(file.isHidden());//是否为隐藏文件
+//        System.out.println(file.length());//文件大小
+
+//        String path = "F:\\比特\\课件\\3.JavaWeb";
+//        File file = new File(path);
+//        file.renameTo(dest);// 剪切文件夹到指定目录
+//        File[] files = file.getParentFile().listFiles();
+//        for(File f : files){//遍历打印当前目录文件，不是递归的
+//            //System.out.println(f);
+//            System.out.println(f.getAbsolutePath());
+//        }
+
+
+//        String parent = "F:\\GitHub\\JavaSE\\测试文件目录";
+//        File file = new File(parent,"text.txt");// 创建文件夹对象
+//        File file = new File(parent,"text.txt");//（java中不要求创建的对象有相应的文件，
+//                                                //只是创建了一个对象，是否在硬盘上存在需要自己具体操作）
+//        file.mkdir();//创建文件夹
+//        File file = new File(parent,"1\\2\\3\\text.txt");
+//        file.mkdirs();// 创建中间路径在不存在的文件夹
+//        file.createNewFile();//创建文件
+//        file.deleteOnExit();//在JVM之后删除
+//        file.delete();//删除文件
+//        File file = new File("F:\\GitHub\\JavaSE\\测试文件目录");
+//        System.out.println(file.exists());//文件夹是否存在
+//        System.out.println(file.isDirectory());// 是否是文件夹
+//        System.out.println(file.getFreeSpace());//获得盘符空闲空间
+//        System.out.println(file.getUsableSpace());//该盘符的可用空间
+//        System.out.println(file.getTotalSpace());//该盘符的总空间
+//        System.out.println(file.getAbsolutePath());// 绝对路径
+//        System.out.println(file.getParent());//父（上级）路径
+//        System.out.println(file.canRead());// 文件是否可读
+//        System.out.println(file.canWrite());//可写
+//        System.out.println(file.canExecute());//可执行
+    }
+}
