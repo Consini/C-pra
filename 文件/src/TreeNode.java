8765b4b5ac8c093e74e590c16b6fe328244cc80b
Consini <2459971164@qq.com>
2020-03-28 22:55:51 +0800

    TreeNode

---
+++ 文件/src/TreeNode.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description TODO
+ * @Author K
+ * @Date 2019/12/1 10:57
+ **/
+public class TreeNode {
+    File file;
+    long totalLength;
+    List<TreeNode> children  = new ArrayList<>();
+}
