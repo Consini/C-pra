70650afa26de709b0bb0cda18126a30dd5441465
Consini <2459971164@qq.com>
2020-04-17 22:30:13 +0800

    每日一题0327_01_罗汉塔

---
+++ 12-01每日一题汇总/src/每日一题0327_01_罗汉塔.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * @Description TODO
+ * @Author K
+ * @Date 2020/4/15 23:12
+ **/
+public class 每日一题0327_01_罗汉塔 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            int n = sc.nextInt();
+
+        }
+    }
+}
